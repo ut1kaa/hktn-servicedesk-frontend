162061df68f78caedc3176c15e04eabf1a5860d7
Misha <utika.nice@gmail.com>
2024-05-31 23:14:42 +0300

    edit imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 import './styles/App.scss';
 import  Home from './pages/Home'
 import Users from './pages/admin/Users';
-import 'bootstrap/dist/css/bootstrap.min.css'
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 const App = () => (
   <div className="App">
